a669a7490823bf5b83806236ee3a549ad26eb421
mgdperera <mgdperera@gmail.com>
2019-11-01 16:13:08 +0530

    complete calculator

--- ReactNativeSampleApp/App.js
+++ ReactNativeSampleApp/App.js
@@ -1,114 +1,129 @@
 
- import React,{Component} from 'react';
+import React, { Component } from 'react';
 
- import{
-   StyleSheet,
-   Text,
-   Button,
-   TextInput,
-   TouchableOpacity,
-   View
- }from 'react-native'
+import {
+  StyleSheet,
+  Text,
+  Button,
+  TextInput,
+  TouchableOpacity,
+  View
+} from 'react-native'
 
-export default class App extends Component{
+export default class App extends Component {
 
-constructor(){
-  super();
-  this.operations = ['del','+','-','*','/']
+  constructor() {
+    super();
+    this.operations = ['del', '+', '-', '*', '/']
 
-}
+  }
 
   state = {
-    calResult: ''
- }
-
-
-PressEqualKey() {
-  const text = this.state.calResult
-}
-
-KeyPress = (text) => {
-
-  if(text == '=')
-  {
-     return this.PressEqualKey();
+    calInput: '',
+    calResult:''
   }
 
-  this.setState({
-    calResult: this.state.calResult + text
-  })
-}
 
-  opertion(operation){
-      switch(operation)
-      {
-          case 'del':
-            let text =this.state.calResult.split('')
-            text.pop()
-            this.setState({
-              calResult:text.join('')
-            })
-            break;
-            case '+':
-            case '-':
-            case '*':
-            case '/':
+  PressEqualKey() {
+    const text = this.state.calInput
+    this.setState({
+      calResult:eval(text)
+    })
+  }
+  
+  Validate(){
+    switch(this.state.calInput.slice(-1))
+    {
+      case'+':
+      case'-':
+      case'/':
+      case'*':
+      case'.':
+       return false;
+       break;
+    }
+    return true;
+  }
 
-                  const lastChar = this.state.calResult.split('').pop()
-                  if(this.operations.indexOf(lastChar)>0) return
 
-                  if(this.state.text == "") return
-                  this.setState({
-                    calResult: this.state.calResult + operation
-                    })
+
+  KeyPress = (text) => {
+
+    if (text == '=') {
+      return this.Validate() && this.PressEqualKey();
+    }
+
+    this.setState({
+      calInput: this.state.calInput + text
+    })
+  }
+
+  opertion(operation) {
+    switch (operation) {
+      case 'del':
+        let text = this.state.calInput.split('')
+        text.pop()
+        this.setState({
+          calInput: text.join('')
+        })
+        break;
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+
+        const lastChar = this.state.calInput.split('').pop()
+        if (this.operations.indexOf(lastChar) > 0) return
+
+        if (this.state.text == "") return
+        this.setState({
+          calInput: this.state.calInput + operation
+        })
+    }
+  }
+
+
+
+
+  render() {
+
+    let rows = []
+    let nums = [[1, 2, 3], [4, 5, 6], [7, 8, 9], ['.', 0, '=']]
+
+    for (let i = 0; i < 4; i++) {
+      let row = []
+      for (let j = 0; j < 3; j++) {
+        row.push(<TouchableOpacity onPress={() => this.KeyPress(nums[i][j])} style={style.btn}>
+          <Text style={style.btnText}>{nums[i][j]}</Text>
+        </TouchableOpacity>)
       }
-  }
+      rows.push(<View style={style.row}>{row}</View>)
+    }
 
+    let operations;
 
-
-
-  render(){
-
-   let rows = []
-   let nums = [[1,2,3],[4,5,6],[7,8,9],['.',0,'=']]
-   
-   for(let i = 0;i<4;i++)
-   {
-     let row = []
-     for(let j =0; j<3;j++)
-     {
-       row.push(<TouchableOpacity onPress={() => this.KeyPress(nums[i][j])} style={style.btn}>
-         <Text style={style.btnText}>{nums[i][j]}</Text>
-       </TouchableOpacity>)
-     }
-     rows.push(<View style={style.row}>{row}</View>)
-   }
-
-let operations;
-
-let ops=[]
-for(let i=0;i<5;i++)
-{
-  ops.push(<TouchableOpacity onPress={() => this.opertion(this.operations[i])} style={style.btn}>
-    <Text style={style.btnText}>{this.operations[i]}</Text>
-  </TouchableOpacity>)
-}
-    return(
+    let ops = []
+    for (let i = 0; i < 5; i++) {
+      ops.push(<TouchableOpacity onPress={() => this.opertion(this.operations[i])} style={style.btn}>
+        <Text style={style.btnText}>{this.operations[i]}</Text>
+      </TouchableOpacity>)
+    }
+    return (
       <View style={style.container}>
-          <View style={style.result}>
-            <Text style={style.resultText}>{this.state.calResult}</Text>
+        <View style={style.result}>
+          <Text style={style.resultText}>{this.state.calInput}</Text>
+        </View>
+        <View style={style.calculation}>
+          <Text style={style.calculationText}>{this.state.calResult}</Text>
+        </View>
+        <View style={style.buttons}>
+          <View style={style.numbers}>
+            {rows}
           </View>
-           <View style={style.calculation}>
-           <Text style={style.calculationText}>1111</Text>
-           </View>
-           <View style={style.buttons}>
-              <View style={style.numbers}>
-                {rows}
-              </View>
-              <View style={style.operations}>
-                 {ops}
-              </View>
-           </View>
+          <View style={style.operations}>
+            {ops}
+          </View>
+        </View>
       </View>
     )
   }
@@ -116,61 +131,63 @@ for(let i=0;i<5;i++)
 
 const style = StyleSheet.create({
   container: {
-    flex:1,
-    justifyContent:'center'
+    flex: 1,
+    justifyContent: 'center'
   },
-  resultText:{
-    fontSize:50,
-    color:'white',
+  resultText: {
+    fontSize: 50,
+    color: 'gray'
   },
-  calculationText:{
-   fontSize:35,
+  calculationText: {
+    fontSize: 35,
+    color:'gray',
+    marginRight:5
   },
-  row:{
-      flexDirection:'row',
-      flex:1,
-      justifyContent:'space-around',
-      alignItems:'stretch'
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'stretch'
   },
-  btnText:{
-   fontSize:35
+  btnText: {
+    fontSize: 35
   },
-  btn:{
-     flex:1,
-     alignItems:'center',
-     alignSelf:'stretch',
-     justifyContent:'center',
-     backgroundColor:'purple'
+  btn: {
+    flex: 1,
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    backgroundColor: 'yellow'
 
   },
-  result:{
-    flex:2,
-    alignItems:'flex-end',
-    justifyContent:'center',
-    backgroundColor:'red'
+  result: {
+    flex: 1,
+    paddingTop:20,
+    backgroundColor:'#fcfcfc',
+    alignItems: 'flex-end',
+    justifyContent: 'center'
+    },
+  calculation: {
+    flex: .7,
+    backgroundColor: '#fcfcfc',
+    alignItems: 'flex-end',
+    justifyContent: 'center',
   },
-  calculation:{
-    flex:1,
-    backgroundColor:'green',
-    alignItems:'flex-end',
-    justifyContent:'center',
-  },
-  buttons:{
-    flex:7,
-    flexDirection:'row',
-    backgroundColor:'purple',
+  buttons: {
+    flex: 7,
+    flexDirection: 'row',
+    backgroundColor: 'yellow',
 
   },
-  numbers:{
-   flex:3,
-   backgroundColor:'yellow',
+  numbers: {
+    flex: 3,
+    backgroundColor: 'yellow',
 
   },
-  operations:{
-      flex:1,
-      backgroundColor:'black',
-      justifyContent:'space-around',
-      alignItems:'stretch'
+  operations: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'stretch'
   }
 
 });
