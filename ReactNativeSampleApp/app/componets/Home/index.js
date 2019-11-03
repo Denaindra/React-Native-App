45e81542a0acd63899123ba9ba8518f3955fe1fb
mgdperera <mgdperera@gmail.com>
2019-11-03 17:03:16 +0530

    add logon button

--- ReactNativeSampleApp/app/componets/Home/index.js
+++ ReactNativeSampleApp/app/componets/Home/index.js
@@ -1,17 +1,34 @@
 import React,{Component} from 'react';
+import styles from './styles'
+
+
 import {
     Text,
     TextInput,
+    Button,
     View
 } from 'react-native'
 
 export default class Home extends Component{
+
+    state = {
+      username: "",
+      password: ""
+    }
+
+    LoginButtonPress()
+    {
+       
+    }
+
     render() {
+        const {heading,input} = styles
         return (
             <View>
-                <Text>Login into the app</Text>
-                <TextInput />
-                <TextInput />
+                <Text style={heading}>Login into this app</Text>
+                <TextInput style={input} />
+                <TextInput secureTextEntry="true" style={input}/>
+                 <Button  title='Login' onPress={() => this.LoginButtonPress()}/>
             </View>
         )
     }
---
+++ ReactNativeSampleApp/app/componets/Home/styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+
+export default StyleSheet.create({
+    heading:{
+        marginTop:30,
+        fontSize:34,
+        textAlign:'center',
+        marginBottom:20
+    },
+    input:{
+        backgroundColor:'green',
+        height:50,
+        marginRight:10,
+        marginLeft:10,
+        marginTop:5,
+        paddingLeft:10
+    }
+});
